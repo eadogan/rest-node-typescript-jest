1ee95d2fb150a53a14e33d69248ac7e02eb2c7b9
eadogan <e.ahmetdogan@gmail.com>
2021-09-26 00:03:51 +0100

    Connect MongoDB via typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
+import 'reflect-metadata'; // this shim is required
+import { cors } from  'cors';
 import express from 'express';
-import routers from '../src/routers/place.routes';
-import connection from '../src/database/connection';
+import { Container } from 'typedi';
+import { useContainer, useExpressServer } from 'routing-controllers';
+import { controllers } from './controllers/index';
 
-const app = express();
-app.use(express.json());
-routers(app);
-connection.create();
+// required by routing-controllers
+useContainer(Container);
+
+const app: express.Express = express();
+
+const routingControllersOptions: any = {
+    defaultErrorHandler: false,
+    cors: true,
+    controllers,
+    interceptors: []
+}
+
+// Wrap server with routing-controllers
+useExpressServer(app, routingControllersOptions);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import http from 'http';
 import app from './app';
+import connection from '../src/database/connection';
 
 const PORT: number = 3000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server listening port ${PORT}...`)
-})
+const handleConncetion = async () => {
+    server.listen(PORT, () => {
+        console.log(`Server listening port ${PORT}...`)
+    })
+}
+
+connection.create(handleConncetion);
