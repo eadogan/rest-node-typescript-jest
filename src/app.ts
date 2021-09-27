21fe37f0edfbaecd405a504de69eb1f5e5ce2fc0
eadogan <e.ahmetdogan@gmail.com>
2021-09-27 19:02:25 +0100

    Refactor code after code-review

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import 'reflect-metadata'; // this shim is required
-import { cors } from  'cors';
-import express from 'express';
-import { Container } from 'typedi';
-import { useContainer, useExpressServer } from 'routing-controllers';
-import { controllers } from './controllers/index';
+import "reflect-metadata"; // this shim is required
+import { cors } from "cors";
+import express from "express";
+import { Container } from "typedi";
+import { useContainer, useExpressServer } from "routing-controllers";
+import { controllers } from "./controllers/index";
 
 // required by routing-controllers
 useContainer(Container);
@@ -11,11 +11,11 @@ useContainer(Container);
 const app: express.Express = express();
 
 const routingControllersOptions: any = {
-    defaultErrorHandler: false,
-    cors: true,
-    controllers,
-    interceptors: []
-}
+  defaultErrorHandler: false,
+  cors: true,
+  controllers,
+  interceptors: [],
+};
 
 // Wrap server with routing-controllers
 useExpressServer(app, routingControllersOptions);
--- src/controllers/place.controller.ts
+++ src/controllers/place.controller.ts
@@ -1,27 +1,31 @@
-import { JsonController, Body, Post, BadRequestError } from 'routing-controllers';
-import { Service } from 'typedi';
-import { Place } from '../entities/place.entity';
-import { PlaceService } from '../services/place.service';
+import {
+  JsonController,
+  Body,
+  Param,
+  Post,
+  BadRequestError,
+  Get,
+} from "routing-controllers";
+import { Service } from "typedi";
+import { Place } from "../entities/place.entity";
+import { PlaceDTO } from "../dto/PlaceDTO";
+import { PlaceService } from "../services/place.service";
+import { InsertResult } from "typeorm";
+import { EntityMapper } from "../utils/mapper.service";
 
-@JsonController('/api')
+@JsonController("/api")
 @Service()
 export class PlaceController {
+  constructor(private readonly placeService: PlaceService) {}
 
-    constructor(
-        private readonly placeService: PlaceService,
-    ) {}
-        
-
-
-    @Post('/place-to-stays/ingest')
-    async postPlace(@Body() place: Place): Promise<Place | undefined | any> {
-        try {
-            return await this.placeService.createPlace(place);
-        } catch (err: any) {
-            throw new BadRequestError(
-                err.detail ?? err.message ?? 'Specific Error'
-            );
-        }
+  @Post("/place-to-stays/ingest")
+  async postPlace(@Body() placeDTO: PlaceDTO): Promise<InsertResult> {
+    try {
+      return await this.placeService.createPlace(
+        EntityMapper.mapTo(Place, placeDTO)
+      );
+    } catch (err: any) {
+      throw new BadRequestError(err.detail ?? err.message ?? "Specific Error");
     }
-
+  }
 }
